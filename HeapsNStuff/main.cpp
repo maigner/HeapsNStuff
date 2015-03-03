d6ce0febfed5b9db8e25177d12d07c374607e125
Martin Aigner <martin@maigner.net>
2015-03-03 10:13:01 +0100

    Initial Commit

---
+++ HeapsNStuff/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HeapsNStuff
+//
+//  Created by Martin Aigner on 03/03/15.
+//  Copyright (c) 2015 Martin Aigner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
